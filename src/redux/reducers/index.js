737d469902e40ff12e65511d86f458d98c3e557f
Pietroski <AugustoPTSantos@gmail.com>
2020-10-06 09:08:05 -0300

    merge from submaster

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 
+import appointments from "./appointments";
 import login from "./login";
+import users from "./users";
 
 export default combineReducers({
   login,
