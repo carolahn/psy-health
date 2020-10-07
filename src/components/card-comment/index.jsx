bf5ea9682913223145dace321cc5a589ae4f1ac2
Alex Miguel <alexmiguel95@gmail.com>
2020-10-07 18:21:10 -0300

    Finish style card comment

---
+++ src/components/card-comment/index.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { Rate } from 'antd';
+import styled from 'styled-components';
+
+const CardComment = ({ image, name, coment, grading }) => {
+    return (
+        <StyledContainerCard>
+            <div>
+                <img src={image} alt="Imagem do paciente" />
+                <h4>{name}</h4>
+            </div>
+            <p>{coment}</p>
+            <div className={'grading'}>
+                <Rate allowHalf value={grading} disabled={true} />
+            </div>
+        </StyledContainerCard>
+    );
+}
+
+export default CardComment;
+
+const StyledContainerCard = styled.div`
+    width: 250px;
+    border: 2px solid #70A3EF;
+    border-radius: 3px;
+    background-color: white;
+    padding: 15px 1% 5px 1%; 
+
+    h4 {
+        font-weight: bold;
+        display: inline-block;
+        vertical-align: top;
+        padding-left: 4px;	
+    }
+
+    img{
+        border: 1px solid #70A3EF;
+        width: 75px;
+        height: 75px;
+        border-radius: 50%;
+    }
+
+    p{
+        font-size: 0.7rem;
+        color: #6E6E6E;
+        margin-bottom: 7px;
+    }
+
+    .grading{
+        text-align: center;
+    }
+`;
