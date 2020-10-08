931c462ed280d58f9ae0924b2ee8cbfc22d29f40
ahncarolina <ahncarolina@gmail.com>
2020-10-08 05:40:28 -0300

    style: set responsive leayout to psi-card-appointment

--- src/components/card-psi-appointment/index.jsx
+++ src/components/card-psi-appointment/index.jsx
@@ -12,14 +12,21 @@ const CardPsiAppointment = ({ type, appointmentData }) => {
     <StyledCard className="card-psi-appointment">
       <Row>
         <Col xs={10} sm={10} md={10} lg={10} xl={10}>
-          CardPsi {appointmentData.patient.name}
+          <div className="col-container">
+            <div className="card-title">{appointmentData.patient.name}</div>
+          </div>
         </Col>
         <Col xs={8} sm={8} md={8} lg={8} xl={8}>
-          {moment(appointmentData.date.start.toLocaleString()).format("DD-MM-YYYY")}
+          <div className="col-container">
+            <div className="card-title">Data da consulta</div>
+            <div>{moment(appointmentData.date.start.toLocaleString()).format("DD-MM-YYYY")}</div>
+          </div>
         </Col>
         <Col xs={6} sm={6} md={6} lg={6} xl={6}>
-          CardPsi
-          {moment(appointmentData.date.start.toLocaleString()).format("HH:mm")}
+          <div className="col-container">
+            <div className="card-title">Horário</div>
+            <div>{moment(appointmentData.date.start.toLocaleString()).format("HH:mm")}</div>
+          </div>
         </Col>
       </Row>
     </StyledCard>
--- src/components/card-psi-appointment/styled.js
+++ src/components/card-psi-appointment/styled.js
@@ -2,7 +2,59 @@ import styled from "styled-components";
 
 export const StyledCard = styled.div`
   max-width: 550px;
+  height: 86px;
+  margin-bottom: 25px;
   background: #f3f3f3;
   border-radius: 5px;
   border: 2px solid #70a3ef;
+  font-family: "Roboto", sans-serif;
+  color: #6e6e6e;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 19px;
+  text-align: center;
+
+  .col-container {
+    height: 86px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+  }
+  .card-title {
+    color: #585858;
+    font-weight: 700;
+    font-size: 20px;
+    line-height: 24px;
+  }
+
+  @media (max-width: 576px) {
+    /* max-width: 260px; */
+    height: 70px;
+    font-size: 14px;
+    line-height: 17px;
+    .col-container {
+      height: 70px;
+    }
+    .card-title {
+      font-weight: 700;
+      font-size: 18px;
+      line-height: 21px;
+    }
+  }
+
+  @media (max-width: 500px) {
+    min-width: 260px;
+    height: 50px;
+    margin-bottom: 15px;
+    font-size: 9px;
+    line-height: 11px;
+    .col-container {
+      height: 50px;
+    }
+    .card-title {
+      font-weight: 700;
+      font-size: 11px;
+      line-height: 13px;
+    }
+  }
 `;
