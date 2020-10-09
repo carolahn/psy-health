2f23f37a47d6db3cdd646ad4abfdb0694e95604e
Alex Miguel <alexmiguel95@gmail.com>
2020-10-09 16:50:59 -0300

    separate the styled component from the carousel-comment

--- src/components/carousel-comment/index.jsx
+++ src/components/carousel-comment/index.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import { Carousel } from 'antd';
+import { StyledDivContainer, StyledCarousel, StyledInnerCarousel} from './styled';
 import CardComment from '../card-comment';
-import styled from 'styled-components';
 import { useWindowSize } from "../../hooks";
 
 const CarouselComment = (props) => {
@@ -68,46 +67,4 @@ const CarouselComment = (props) => {
 }
 
 export default CarouselComment;
-
-const StyledDivContainer = styled.div`
-    width: 100%;
-    background-color: #F3F3F3;
-    border-top: 3px solid #70A3EF;
-    border-bottom: 3px solid #70A3EF;
-
-    h1{
-        color: #717171;
-        text-align: center;
-        margin-top: 14px;
-    }
-`;
-
-const StyledCarousel = styled(Carousel)`
-    min-height: 325px;
-    width: 100%;
-    display: flex !important;
-    flex-flow: row nowrap;
-    justify-content: center;
-    align-items: center;
-
-    .slick-dots li button{
-        border: 1px solid #70A3EF;
-        height: 10px;
-        width: 20px;
-        border-radius: 3px;
-    }
-
-    .slick-dots li.slick-active button{
-        background-color: #70A3EF;
-    }
-`;
-
-const StyledInnerCarousel = styled.div`
-    width: 100%;
-    display: flex !important;
-    flex-flow: row nowrap;
-    justify-content: space-evenly;
-    align-items: center;
-    margin-bottom: 25px;
-`;
 
