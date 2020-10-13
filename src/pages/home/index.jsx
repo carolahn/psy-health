8a5376bcbe58f0381def8d2b37abac550488fb1e
ahncarolina <ahncarolina@gmail.com>
2020-10-13 12:20:44 -0300

    feat: add home

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,14 +1,27 @@
-import React from "react";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 
 import BannerOperation from "../../components/banner-operation";
 import BannerSearch from "../../components/banner-search";
+import DepoimentsBanner from "../../containers/depoiments-banner";
 import { MainContainer } from "./styled";
 
 const Home = () => {
+  const [listComments, setListComments] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://psy-health-api.herokuapp.com/depoiments").then(({ data }) => {
+      setListComments([...data]);
+    });
+  }, []);
+
+  console.log(listComments);
+
   return (
     <MainContainer className="home-page">
       <BannerSearch />
       <BannerOperation />
+      {/* {listComments && <DepoimentsBanner listComments={listComments} />} */}
     </MainContainer>
   );
 };
