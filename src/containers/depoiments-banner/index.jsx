71f88aa5a87226bdc1f45451409cc96113533963
Alex Miguel <alexmiguel95@gmail.com>
2020-10-13 22:48:10 -0300

    Changed the depoiments-banner container logic. It needs to receive an array of objects with these properties: image, name, coment, grading.

--- src/containers/depoiments-banner/index.jsx
+++ src/containers/depoiments-banner/index.jsx
@@ -1,41 +1,91 @@
 import React from "react";
-
-import CarouselComment from "../../components/carousel-comment";
 import { useWindowSize } from "../../hooks";
+import CardComment from "../../components/card-comment";
+import { StyledDivContainer, StyledCarousel, StyledInnerCarousel } from "./styled";
 
-// Recebe um array com todos os depoimentos
-const DepoimentsBanner = (props) => {
+const DepoimentsBanner = ({ listAllComments }) => {
   const [width] = useWindowSize();
   const listComments = [];
 
   if (width >= 950) {
     while (listComments.length < 9) {
-      const random = Math.round(Math.random() * (props.listComments.length - 1));
-
-      //Verifica se não tem um comentário do userId na lista. Se não encontrar, ele adiciona o comentario na lista
-      if (
-        listComments.find((position) => position.userId === props.listComments[random].userId) ===
-        undefined
-      ) {
-        listComments.push(props.listComments[random]);
-      }
+      const random = Math.round(Math.random() * (listAllComments.length - 1));
+      listComments.push(listAllComments[random]);
     }
 
-    return <CarouselComment listComments={listComments} />;
+    return(
+      <StyledDivContainer>
+        <h1>Depoimentos</h1>
+        <StyledCarousel>
+          {
+            listComments.map((e, i, arr) => {
+              if (i % 3 === 0 && i <= arr.length - (i % 3))
+              return (
+                <StyledInnerCarousel key={i}>
+                {
+                  arr[i] && (
+                    <CardComment
+                      image={arr[i].image}
+                      name={arr[i].name}
+                      coment={arr[i].coment}
+                      grading={arr[i].grading}
+                    />
+                  )
+                }
+                {
+                  arr[i + 1] && (
+                    <CardComment
+                      image={arr[i + 1].image}
+                      name={arr[i + 1].name}
+                      coment={arr[i + 1].coment}
+                      grading={arr[i + 1].grading}
+                    />
+                  )
+                }
+                {
+                  arr[i + 2] && (
+                    <CardComment
+                      image={arr[i + 2].image}
+                      name={arr[i + 2].name}
+                      coment={arr[i + 2].coment}
+                      grading={arr[i + 2].grading}
+                    />
+                  )
+                }
+                </StyledInnerCarousel>
+              );
+            })
+          }
+        </StyledCarousel>
+      </StyledDivContainer>
+    );
   } else {
-    while (listComments.length < 3) {
-      const random = Math.round(Math.random() * (props.listComments.length - 1));
-
-      //Verifica se não tem um comentário do userId na lista. Se não encontrar, ele adiciona o comentario na lista
-      if (
-        listComments.find((position) => position.userId === props.listComments[random].userId) ===
-        undefined
-      ) {
-        listComments.push(props.listComments[random]);
-      }
+    while (listComments.length < 3) { 
+      const random = Math.round(Math.random() * (listAllComments.length - 1));
+      listComments.push(listAllComments[random]);
     }
 
-    return <CarouselComment listComments={listComments} />;
+    return(
+      <StyledDivContainer>
+        <h1>Depoimentos</h1>
+        <StyledCarousel>
+          {
+            listComments.map((e, key) => {
+              return(
+                <StyledInnerCarousel key={key}>
+                  <CardComment
+                    image={e.image}
+                    name={e.name}
+                    coment={e.coment}
+                    grading={e.grading}
+                  />
+                </StyledInnerCarousel>
+              )
+            })
+          }
+        </StyledCarousel>
+      </StyledDivContainer>
+    );
   }
 };
 
